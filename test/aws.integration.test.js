b28f2d041029733222e8d8fdac3833fe872ad767
mkotsollaris <mkotsollari@gmail.com>
2019-06-15 17:19:14 -0700

    updated test

--- test/aws.integration.test.js
+++ test/aws.integration.test.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 it('endpoint works', async (done) => {
   const endpoint = 'https://870m93mis4.execute-api.us-west-2.amazonaws.com/default/Test';
-  const response = await makeRequest(endpoint);
+  const response = await makeRequest(endpoint); 
+  expect(response).toBeTruthy();
+  expect.assertions(1);
+  done();
+});
+
+it('endpoint works', async (done) => {
+  const endpoint = 'https://870m93mis4.execute-api.us-west-2.amazonaws.com/default/Test';
+  const response = await makeRequest(endpoint); 
   expect(response).toBeFalsy();
   expect.assertions(1);
   done();
